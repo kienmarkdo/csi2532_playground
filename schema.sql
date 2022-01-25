56e59730dca7f5362ce64a842b9cfbcaa2e2f32e
Kien Do <kienmarkdo@gmail.com>
2022-01-24 20:25:18 -0500

    reverted schema.sql back to original code

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
-CREATE TABLE voitures (
-  marque varchar(255),
-  annee integer,
-  model varchar(255)
+CREATE TABLE department (
+  dept_name varchar(20),
+  building varchar(15),
+  budget numeric(12,2) check (budget > 0),
+  primary key (dept_name)
+);
+
+CREATE TABLE course (course_id    varchar(8),
+  title varchar(50),
+  dept_name varchar(20),
+  credits numeric(2,0) check (credits > 0),
+  primary key (course_id),
+  foreign key (dept_name) references department
+  on delete set null
 );
