0858f3ab2424634b5204777469f6144b1b2c470f
Kien Do <kienmarkdo@gmail.com>
2022-02-18 22:51:13 -0500

    terminé le labo 5

---
+++ migrations/20220214175700-ajout-PK.sql
@@ -0,0 +1,2 @@
+ALTER TABLE courses
+ADD PRIMARY KEY(id); -- note that in other SQL languages, you may need ADD CONSTRAINT PRIMARY KEY
---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE courses(
+	course_name varchar(255),
+	semester varchar(255),
+	professor_name varchar(255)
+);
+
+
+-- ALTER TABLE courses
+-- ADD CONSTRAINT PRIMARY KEY(id); -- note that with Postgres, you do not need the word CONSTRAINT
