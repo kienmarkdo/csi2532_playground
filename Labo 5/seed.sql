6a1a94c8daa48a897f862e6df596a7aaaa3d65dc
Kien Do <67518620+kienmarkdo@users.noreply.github.com>
2022-04-26 16:21:47 -0400

    Add files via upload

---
+++ Labo 5/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO courses(course_name, semester, professor_name)
+VALUES('CSI2532','W-2020','A'),
+('CSI2532','W-2020','A'),
+('CSI2532','W-2029','A'),
+('CSI2532','W-2021','A'),
+('CSI2532','W-2022','A'),
+('CSI2532','W-2024','A'),
+('CSI2532','W-2022','A');
+
+-- -- ALTER TABLE alters the table. For example, you want to add a new column into the table.
+-- ALTER TABLE courses
+-- ADD id integer;
